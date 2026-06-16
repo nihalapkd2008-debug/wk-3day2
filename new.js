8569d3df6209bead75191b7e19181c51bd12a958
nihalapkd2008-debug <nihalapkd2008@gmail.com>
2026-06-16 12:11:36 +0530

    first commit

--- new.js
+++ new.js
@@ -33,3 +33,41 @@ console.log("==Dry Run Loop Flow==");
 for(let i = 1; i <= 5; i++){
   console.log(i);
 }
+console.log("==Beginner Logic Problem 1==");
+
+console.log("Even or Odd");
+
+let num = 8;
+if (num % 2 === 0){
+
+  console.log(num + " is Even");
+
+}
+else {
+  console.log(num + " is Odd");
+
+}
+console.log("==Beginner Logic Problem 2==");
+
+console.log("Positive or Negative");
+
+let value = -10;
+if (value > 0) {
+  console.log("Positive");
+}
+else {
+  console.log("Negative");
+}
+
+console.log("==Beginner Logic Problem 3==");
+console.log("Largest NUmber");
+
+let a = 10;
+let b = 25;
+ if (a > b) {
+
+  console.log(a + " is Largest");
+ }
+ else{
+  console.log(b + " is Largest");
+ }
